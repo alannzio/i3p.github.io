8e2234e1824f8f1a7a6a1c14ac0fda20bb8fbec1
Alan Santana <119264746+alannzio@users.noreply.github.com>
2023-05-09 17:06:28 -0600

    Update btn.js

--- APP/btn.js
+++ APP/btn.js
@@ -4,3 +4,7 @@ var startButton = document.getElementById("startButton");
 startButton.addEventListener("click", function() {
   video.play();
 });
+
+video.addEventListener('error', function() {
+  console.error('Error al cargar el video');
+});
